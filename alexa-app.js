9e1a67efa6d62500169c6847aaa956ffaadf8740
Plinio Cardoso <pliniocardoso@pliniocardoso.local>
2018-06-26 22:47:46 +0100

    Added responses

--- alexa-app.js
+++ alexa-app.js
@@ -1,44 +1,108 @@
-exports.handler = function( event, context ) {
+var https = require('https');
 
-    var http = require( 'http' );
+exports.handler = (event, context) => {
 
-    var url = 'Your Weather Underground link with API key goes here!';
+    try {
 
-    http.get( url, function( response ) {
+        if (event.session.new) {
+            // New Session
+            console.log("NEW SESSION")
+        }
 
-        var data = '';
+        switch (event.request.type) {
 
-        response.on( 'data', function( x ) { data += x; } );
+            case "LaunchRequest":
+                // Launch Request
+                console.log(`LAUNCH REQUEST`)
+                context.succeed(
+                    generateResponse(
+                        buildSpeechletResponse("Welcome to an Alexa Skill, this is running on a deployed lambda function", true),
+                        {}
+                    )
+                )
+                break;
 
-        response.on( 'end', function() {
+            case "IntentRequest":
+                // Intent Request
+                console.log(`INTENT REQUEST`)
 
-            var json = JSON.parse( data );
+                switch(event.request.intent.name) {
+                    case "GetHousesForSale":
+                        var location = event.request.intent.slots.location.value;
+                        var endpoint = "https://tiny-cow-77.localtunnel.me/v1/adverts/" + location;
+                        var body = "";
+                        https.get(endpoint, (response) => {
+                            response.on('data', (chunk) => { body += chunk })
+                            response.on('end', () => {
+                                var data = JSON.parse(body)
+                                
+                                if (location == undefined) {
+                                    context.succeed(
+                                        generateResponse(
+                                            buildSpeechletResponse('Sorry, I don\'t know that location', true),
+                                            {}
+                                        )
+                                    )
+                                } else {
+                                    if (data.length == 0) {
+                                        context.succeed(
+                                            generateResponse(
+                                                buildSpeechletResponse('I didn\'t find any house for sale in ' + location, true),
+                                                {}
+                                            )
+                                        )
+                                    } else {
+                                        context.succeed(
+                                            generateResponse(
+                                                buildSpeechletResponse('I found ' + data.length + ' houses in ' + location, true),
+                                                {}
+                                            )
+                                        )
+                                }   
+                                }
+                            })
+                        })
+                        break;
 
-            var text = 'According to Weather Underground outside with wind chill the temperature feels like ';
-            text+=json.current_observation.feelslike_f+" degrees fahrenheit.";
-            output( text, context );
+                    default:
+                        throw "Invalid intent"
+                }
 
-        } );
+                break;
 
-    } );
+            case "SessionEndedRequest":
+                // Session Ended Request
+                console.log(`SESSION ENDED REQUEST`)
+                break;
 
-};
+            default:
+                context.fail(`INVALID REQUEST TYPE: ${event.request.type}`)
 
-function output( text, context ) {
+        }
 
-    var response = {
+    } catch(error) { context.fail(`Exception: ${error}`) }
+
+}
+
+// Helpers
+function buildSpeechletResponse (outputText, shouldEndSession) {
+
+    return {
         outputSpeech: {
             type: "PlainText",
-            text: text
+            text: outputText
         },
-        card: {
-            type: "Simple",
-            title: "Thornton Windchill",
-            content: text
-        },
-        shouldEndSession: true
-    };
+        shouldEndSession: shouldEndSession
+    }
 
-    context.succeed( { response: response } );
+}
+
+function generateResponse (speechletResponse, sessionAttributes) {
+
+    return {
+        version: "1.0",
+        sessionAttributes: sessionAttributes,
+        response: speechletResponse
+    }
 
 }
